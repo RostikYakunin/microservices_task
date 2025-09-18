0c60c5493f5bac117d2fbf98b21528b2390f2196
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2025-09-18 12:59:14 +0300

    minor fix

--- resource-service/src/main/java/com/example/resource_service/services/SongServiceClient.java
+++ resource-service/src/main/java/com/example/resource_service/services/SongServiceClient.java
@@ -3,7 +3,6 @@ package com.example.resource_service.services;
 import com.example.resource_service.dtos.SongMetadataRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -12,14 +11,13 @@ import org.springframework.web.client.RestClient;
 @RequiredArgsConstructor
 public class SongServiceClient {
     private final RestClient restClient;
-    private final DiscoveryClient discoveryClient;
 
     @Value("${song-service.base-url}")
     private String baseUrl;
 
     public void createSongMetadata(SongMetadataRequest body) {
         restClient.post()
-                .uri(getBaseUrl() + "/songs")
+                .uri(baseUrl + "/songs")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(body)
                 .retrieve()
@@ -28,23 +26,8 @@ public class SongServiceClient {
 
     public void deleteByCsv(String csv) {
         restClient.delete()
-                .uri(getBaseUrl() + "/songs?id={csv}", csv)
+                .uri(baseUrl + "/songs?id={csv}", csv)
                 .retrieve()
                 .toBodilessEntity();
-    }
-
-    private String getBaseUrl() {
-        var instances = discoveryClient.getInstances("song-service");
-        if (instances.isEmpty()) {
-            throw new IllegalStateException("Song service is not registered in Eureka");
-        }
-
-        if (!baseUrl.contains("localhost")) return baseUrl;
-
-        var instance = instances.get(0);
-        var host = instance.getHost();
-        var port = instance.getPort();
-
-        return "http://" + host + ":" + port;
     }
 }
